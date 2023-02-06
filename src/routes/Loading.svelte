1318181186491250ffa20c683f5f914401dc7334
Towsif Kafi <kafitowsif@gmail.com>
2023-02-06 19:25:56 +0600

    initial commit

---
+++ src/routes/Loading.svelte
@@ -0,0 +1,10 @@
+<main>
+    <h2>Loading</h2>
+    {#if params && params.message}
+        <p id="loadingmessage">Message is {params.message}</p>
+    {/if}
+</main>
+
+<script>
+    export let params = null
+</script>
